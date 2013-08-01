5634f8cd8349e60905c054fb6b97540864b75571
bowhan <bo.han@umassmed.edu>
2013-07-31 20:57:50 -0400

    reduce -n if too high

--- src/tailor_map.hpp
+++ src/tailor_map.hpp
@@ -43,7 +43,7 @@ and the sequences are reported under "TL:Z:" in the optional fields.
 				("input,i", boost::program_options::value<std::string>(&inputFastq)->required(), "Input fastq file")
 				("index,p", boost::program_options::value<std::string>(&indexPrefix)->required(), "Prefix of the index")
 				("output,o", boost::program_options::value<std::string>(&outputSAM)->default_value(std::string{"stdout"}), "Output SAM file, stdout by default ")
-				("thread,n", boost::program_options::value<std::size_t>(&nthread)->default_value(1), "Number of thread to use")
+				("thread,n", boost::program_options::value<std::size_t>(&nthread)->default_value(1), "Number of thread to use; if the number is larger than the core available, it will be adjusted automatically")
 				("minLen,l", boost::program_options::value<int>(&minLen)->default_value(18), "minimal length of exact match (prefix match) allowed")
 				;
 		boost::program_options::variables_map vm;
@@ -60,6 +60,7 @@ and the sequences are reported under "TL:Z:" in the optional fields.
 		std::cerr << opts << std::endl;
 		exit (1);
 	}
+
 	/** check index **/
 	if (indexPrefix.back () != '.') {
 		indexPrefix += '.';
@@ -83,6 +84,12 @@ and the sequences are reported under "TL:Z:" in the optional fields.
 			std::cerr << "Error: cannot creat output file " << outputSAM << ".\nPlease double check.\nExiting..." << std::endl;
 			exit (1);
 		}
+	}
+	/** check thread **/
+	auto nCore = boost::thread::hardware_concurrency();
+	if ( nthread > nCore) {
+		std::cerr << "Warning: the number of threads set (" << nthread << ") is larger than the number of cores available (" << nCore << ") in this machine.\nSo reset -n=" << nCore << std::endl;
+		nthread = nCore;
 	}
 	/** execute mapping **/
 	tailing2 (indexPrefix, inputFastq, out, nthread, minLen);
