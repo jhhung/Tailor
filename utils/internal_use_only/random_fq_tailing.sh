ef1cb7775e169179319bd29e5965a121b6508c77
bowhan <bo.han@umassmed.edu>
2013-11-04 13:57:54 -0500

    modify random_fq_tailing to generate 1-3 nt, not 0-3

--- utils/internal_use_only/random_fq_tailing.sh
+++ utils/internal_use_only/random_fq_tailing.sh
@@ -6,4 +6,4 @@
 
 # randomly append 1-3 nucleotide to the end of fastq
 
-gawk 'BEGIN{nt[0]="A"; nt[1]="C"; nt[2]="G"; nt[3]="T"; srand(systime() + PROCINFO["pid"]); C=0} {ranLen=int(rand()*100)%4; ranTail=""; for (i=1;i<=ranLen;++i) ranTail=ranTail""nt[int(rand()*100)%4]; print "@"C++"."(ranTail==""?"p":ranTail); getline; printf "%s%s", $0, ranTail; getline; getline; printf "\n+\n%s", $0; for (i=1;i<=ranLen;++i) printf "I"; printf "\n"}' $1 > ${1%.f[qa]*}.randomeTailed.fq
+gawk 'BEGIN{nt[0]="A"; nt[1]="C"; nt[2]="G"; nt[3]="T"; srand(systime() + PROCINFO["pid"]); C=0} {ranLen=(int(rand()*100)%3+1); ranTail=""; for (i=1;i<=ranLen;++i) ranTail=ranTail""nt[int(rand()*100)%4]; print "@"C++"."(ranTail==""?"p":ranTail); getline; printf "%s%s", $0, ranTail; getline; getline; printf "\n+\n%s", $0; for (i=1;i<=ranLen;++i) printf "I"; printf "\n"}' $1 > ${1%.f[qa]*}.randomeTailed.fq
